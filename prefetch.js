8e9608626ee762e29b6415d2415ec5fa1569818e
John Kelly <johnharrykelly@gmail.com>
2024-11-26 06:18:25 +0000

    more speeed

--- prefetch.js
+++ prefetch.js
@@ -1,10 +1,12 @@
-console.log("Prefetch enabled")
+console.log("prefetch enabled");
 
 const links = document.querySelectorAll('a');
 
 links.forEach(anchor => {
   const href = anchor.getAttribute('href');
-  if (href) {
+  if (href && !href.startsWith("mailto") && !href.startsWith("http")) {
+    console.log(`prefetching '${href}'`);
+
     const linkElement = document.createElement('link');
     linkElement.rel = 'prefetch';
     linkElement.href = href;
