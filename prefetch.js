af44f2f0007ccc51f3452f39df9d5b21a113329e
John Kelly <johnharrykelly@gmail.com>
2024-11-26 06:03:45 +0000

    Prefetch

---
+++ prefetch.js
@@ -0,0 +1,12 @@
+const links = document.querySelectorAll('a');
+
+links.forEach(anchor => {
+  const href = anchor.getAttribute('href');
+  if (href) {
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'prefetch';
+    linkElement.href = href;
+
+    document.head.appendChild(linkElement);
+  }
+});
